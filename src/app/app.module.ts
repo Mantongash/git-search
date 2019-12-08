af6bc68ff4695def7796d13b13475b55a4f74805
Mantongash <you@example.com>
2019-12-08 10:46:41 +0300

    Imported HttpClientModule in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { UsersService } from './services/users.service';
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
