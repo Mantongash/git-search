112707afe45bfc118bb11e42c0fd38f8a3142c54
Mantongash <you@example.com>
2019-12-08 10:42:41 +0300

    Changed HttpClientModule to HttpClient

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClientModule, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import "rxjs/add/operator/map";
 
 @Injectable({
@@ -9,7 +9,7 @@ export class UsersService {
   private username: string;
   private clientid = "Iv1.06204863a8f8ba72";
   private clientsecret = "862ea922db5c5eac4b17569951f74822fcc60054";
-  constructor(private http: HttpClientModule) {
+  constructor(private http: HttpClient) {
     console.log("Service is now ready");
     this.username = "Mantongash";
   }
