ed33a9e8b2759eada2d8ce817b4c70fbcb3294bb
Mantongash <you@example.com>
2019-12-09 02:27:55 +0300

    Imported FormsModule in the app.module file and included it in the declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,7 +10,7 @@ import { UsersService } from "./services/users.service";
 
 @NgModule({
   declarations: [AppComponent, UsersComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [UsersService],
   bootstrap: [AppComponent]
 })
