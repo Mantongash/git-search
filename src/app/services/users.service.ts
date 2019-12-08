00386b9ea53ba24d35de5a63f1c3e62f6eee09ae
Mantongash <you@example.com>
2019-12-08 10:25:44 +0300

    Added a console.log() message to check if our service is running

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -9,5 +9,7 @@ export class UsersService {
   private username: string;
   private clientid = "Iv1.06204863a8f8ba72";
   private clientsecret = "862ea922db5c5eac4b17569951f74822fcc60054";
-  constructor(private http: HttpClientModule) {}
+  constructor(private http: HttpClientModule) {
+    console.log("Service is now ready")
+  }
 }
